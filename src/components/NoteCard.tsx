81bd01795b7c35095dee04138ba4460f778d894d
Emerson Fully <63175980+emersonfully@users.noreply.github.com>
2024-02-10 12:10:43 -0300

    finish the modal style

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { X } from "lucide-react";
 
 interface NoteCardProps {
   note: {
@@ -12,18 +13,21 @@ interface NoteCardProps {
 export function NoteCard({ note }: NoteCardProps) {
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <Dialog.Trigger className="relative flex flex-col gap-3 space-y-3 overflow-hidden rounded-md bg-slate-800 p-5 text-left outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-200">
           {note.date.toISOString()}
         </span>
         <p className="text-sm leading-6 text-slate-300">{note.content}</p>
-        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+        <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0"></div>
       </Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Overlay className="fixed inset-0 bg-black/50" />
 
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className=" fixed left-1/2 top-1/2 flex h-[60vh] w-full max-w-[640px] -translate-x-1/2 -translate-y-1/2 flex-col overflow-hidden rounded-md bg-slate-700 outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5 " />
+          </Dialog.Close>
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-200">
               {formatDistanceToNow(note.date, {
@@ -33,6 +37,16 @@ export function NoteCard({ note }: NoteCardProps) {
             </span>
             <p className="text-sm leading-6 text-slate-300">{note.content}</p>
           </div>
+          <button
+            type="button"
+            className="group: group w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none"
+          >
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline">
+              apagar essa nota
+            </span>
+            ?
+          </button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
